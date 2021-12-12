9612dd56ac8a383a550def421fbb5fdeb960636f
Sema <semen.shilovski@yandex.ru>
2021-12-12 19:04:39 +0300

    add sql-sends

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,73 @@
 import Database.DataBase;
 import Persons.Student;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        Student student = new Student();
-        Map<Integer,Student> allStudents = new HashMap<Integer,Student>();
-        allStudents = student.fillListOfStudents();
-        for (Map.Entry entry : allStudents.entrySet()) {
-            System.out.println(entry.getValue()+" "+ entry.getKey());
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            System.out.println("========================");
+            System.out.println("Консольное меню.");
+            System.out.println("Введите 0 для выхода из приложения.");
+            System.out.println("Введите 1 для добавления студента.");
+            System.out.println("Введите 2 для изменения информации о студенте.");
+            System.out.println("========================");
+            int action = scanner.nextInt();
+            clearScreen();
+            switch (action){
+                case 0:
+                    System.exit(0);
+                    break;
+                case 1:
+                    System.out.println("Введите № студенческого билета студента:");
+                    int id = scanner.nextInt();
+                    System.out.println("Введите имя студента:");
+                    scanner.nextLine();
+                    String name = scanner.nextLine();
+                    System.out.println("Введите возраст студента:");
+                    int age = scanner.nextInt();
+                    System.out.println("Введите факультет студента:");
+                    scanner.nextLine();
+                    String faculty = scanner.nextLine();
+                    System.out.println("Введите курс студента:");
+                    int course = scanner.nextInt();
+                    System.out.println("Введите 0,если у студента нет стипендии, введите 1, если у студента есть стипендия:");
+                    int scholarship = scanner.nextInt();
+                    System.out.println("Введите средний балл студента:");
+                    double score = scanner.nextDouble();
+                    try{
+                        Student student = new Student(id,name,age,faculty,course,scholarship,score);
+                        student.writeStudentInBD(student);
+                    }catch (Exception e){
+                        System.out.println(e.getMessage());
+                        System.exit(0);
+                    }
+                    break;
+                case 2:
+                    System.out.println("Введите номер студенческого студента:");
+                    int find = scanner.nextInt();
+                    System.out.println("Введите значение, которое хотите изменить:");
+                    System.out.println("name/age/faculty/course/scholarship/score");
+                    scanner.nextLine();
+                    String act = scanner.nextLine();
+                    System.out.println("Введите значение на которое хотите изменить.");
+                    String mean = scanner.nextLine();
+                    Student student = new Student();
+                    student.updateInfoInBD(act,mean,find);
+                    break;
+                case 3:
+                case 4:
+            }
+        }
+    }
+    public static void clearScreen() {
+        for(int i = 0; i < 60; i++){
+            System.out.println();
         }
     }
 }
--- src/Persons/Student.java
+++ src/Persons/Student.java
@@ -13,6 +13,10 @@ public class Student {
     DataBase dataBase = new DataBase();
     Connection con = dataBase.getDbConnection();
 
+    public String getName() {
+        return name;
+    }
+
     @Override
     public String toString() {
         return "Student{" +
@@ -22,45 +26,142 @@ public class Student {
                 ", scholarship=" + scholarship +
                 ", score=" + score +
                 ", name='" + name + '\'' +
-                ", faculty='" + faculty ;
+                ", faculty='" + faculty;
     }
 
-    public Student(Integer id, String name, Integer age, String faculty, Integer course, int scholarship, double score) throws SQLException, ClassNotFoundException {
-    this.id = id;
-    this.name = name;
-    this.age = age;
-    this.faculty = faculty;
-    this.course = course;
-    this.scholarship = scholarship;
-    this.score = score;
+    public Student(Integer id, String name, Integer age, String faculty, Integer course, int scholarship, double score) throws Exception {
+        if( id < 0 || age < 0 || course < 1 || course > 6 || scholarship > 1 || scholarship < 0 ) throw new Exception("error value");
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.faculty = faculty;
+        this.course = course;
+        this.scholarship = scholarship;
+        this.score = score;
     }
+
     public Student() throws SQLException, ClassNotFoundException {
 
     }
 
-
     public void createStudent(Integer stdid, String namest, Integer agest, String facultyst, Integer coursest, int scholarshipst, double scorest) throws SQLException, ClassNotFoundException {
-       String sql = "INSERT INTO student (id, name, age, faculty, course, scholarship, score) VALUES(?,?,?,?,?,?,?)";
+        String sql = "INSERT INTO student (id, name, age, faculty, course, scholarship, score) VALUES(?,?,?,?,?,?,?)";
         PreparedStatement preparedStatement = con.prepareStatement(sql);
-        preparedStatement.setInt(1,stdid);
-        preparedStatement.setString(2,namest);
-        preparedStatement.setInt(3,agest);
-        preparedStatement.setString(4,facultyst);
-        preparedStatement.setInt(5,coursest);
-        preparedStatement.setInt(6,scholarshipst);
-        preparedStatement.setDouble(7,scorest);
+        preparedStatement.setInt(1, stdid);
+        preparedStatement.setString(2, namest);
+        preparedStatement.setInt(3, agest);
+        preparedStatement.setString(4, facultyst);
+        preparedStatement.setInt(5, coursest);
+        preparedStatement.setInt(6, scholarshipst);
+        preparedStatement.setDouble(7, scorest);
         preparedStatement.executeUpdate();
     }
 
-    public Map<Integer,Student> fillListOfStudents() throws SQLException, ClassNotFoundException {
-        Map<Integer,Student> allStudents = new HashMap<Integer,Student>();
+    public Map<Integer, Student> fillListOfStudents() throws SQLException, ClassNotFoundException {
+        Map<Integer, Student> allStudents = new HashMap<Integer, Student>();
         Statement statement = con.createStatement();
         String sql_send = "select * from student order by id";
         ResultSet resultSet = statement.executeQuery(sql_send);
-        while(resultSet.next()){
-            Student st = new Student(resultSet.getInt("id"),resultSet.getString("name"),resultSet.getInt("age"),resultSet.getString("faculty"),resultSet.getInt("course"),resultSet.getInt("scholarship"),resultSet.getDouble("score"));
-            allStudents.put(resultSet.getInt("id"),st);
+        while (resultSet.next()) {
+            try{
+                Student st = new Student(resultSet.getInt("id"), resultSet.getString("name"), resultSet.getInt("age"), resultSet.getString("faculty"), resultSet.getInt("course"), resultSet.getInt("scholarship"), resultSet.getDouble("score"));
+                allStudents.put(resultSet.getInt("id"), st);
+            }catch (Exception e){
+
+            }
+
         }
         return allStudents;
     }
+
+    public Student getStudent(int id) throws SQLException, ClassNotFoundException {
+        String sql = "SELECT * FROM student WHERE id = ?";
+        PreparedStatement preparedStatement = con.prepareStatement(sql);
+        preparedStatement.setInt(1, id);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+        Student student = new Student();
+        try{
+            Student st = new Student(resultSet.getInt("id"), resultSet.getString("name"), resultSet.getInt("age"), resultSet.getString("faculty"), resultSet.getInt("course"), resultSet.getInt("scholarship"), resultSet.getDouble("score"));
+            return st;
+        }catch (Exception e){
+
+        }
+
+        return student;
+    }
+
+    public void writeStudentInBD(Student student) throws SQLException {
+        String sqlsend = "INSERT INTO student (id, name, age, faculty, course, scholarship, score) VALUES(?,?,?,?,?,?,?)";
+        PreparedStatement preparedStatement = con.prepareStatement(sqlsend);
+        preparedStatement.setInt(1, student.id);
+        preparedStatement.setString(2, student.name);
+        preparedStatement.setInt(3, student.age);
+        preparedStatement.setString(4, student.faculty);
+        preparedStatement.setInt(5, student.course);
+        preparedStatement.setInt(6, student.scholarship);
+        preparedStatement.setDouble(7, student.score);
+        preparedStatement.executeUpdate();
+    }
+
+    public void deleteStudentFromBD(int id) throws SQLException {
+        String sql = "DELETE from student where id = ?";
+        PreparedStatement preparedStatement = con.prepareStatement(sql);
+        preparedStatement.setInt(1,id);
+        preparedStatement.executeUpdate();
+    }
+
+    public void updateInfoInBD(String action,String meaning, int id) throws Exception {
+        String sql = "";
+        if( action.equals("name") ){
+            sql = "update student set name = ? where id = ?";
+            PreparedStatement preparedStatement = con.prepareStatement(sql);
+            preparedStatement.setString(1,meaning);
+            preparedStatement.setInt(2,id);
+            preparedStatement.executeUpdate();
+        }
+        else if( action.equals("age") ){
+            sql = "update student set age = ? where id = ?";
+            int i = Integer.parseInt(meaning);
+            PreparedStatement preparedStatement = con.prepareStatement(sql);
+            preparedStatement.setInt(1,i);
+            preparedStatement.setInt(2,id);
+            preparedStatement.executeUpdate();
+        }
+        else if( action.equals("faculty") ){
+            sql = "update student set faculty = ? where id = ?";
+            PreparedStatement preparedStatement = con.prepareStatement(sql);
+            preparedStatement.setString(1,meaning);
+            preparedStatement.setInt(2,id);
+            preparedStatement.executeUpdate();
+        }
+        else if( action.equals("course") ){
+            sql = "update student set course = ? where id = ?";
+            PreparedStatement preparedStatement = con.prepareStatement(sql);
+            int i = Integer.parseInt(meaning);
+            preparedStatement.setInt(1,i);
+            preparedStatement.setInt(2,id);
+            preparedStatement.executeUpdate();
+        }
+        else if( action.equals("scholarship") ){
+            sql = "update student set scholarship = ? where id = ?";
+            PreparedStatement preparedStatement = con.prepareStatement(sql);
+            int i = Integer.parseInt(meaning);
+            preparedStatement.setInt(1,i);
+            preparedStatement.setInt(2,id);
+            preparedStatement.executeUpdate();
+        }
+        else if( action.equals("score") ){
+            sql = "update student set score = ? where id = ?";
+            PreparedStatement preparedStatement = con.prepareStatement(sql);
+            double i = Double.parseDouble(meaning);
+            preparedStatement.setDouble(1,i);
+            preparedStatement.setInt(2,id);
+            preparedStatement.executeUpdate();
+        }else{
+            Exception exception;
+            throw new Exception("error");
+        }
+    }
+
 }
